14e40597c8c2134b0948d36068dbca3f54093e3b
conradmauritz <79597329+conradmauritz@users.noreply.github.com>
2021-11-26 22:56:23 +0100

    Update TableConsistencyCheckReport.py

--- TableConsistencyCheckReport/TableConsistencyCheckReport.py
+++ TableConsistencyCheckReport/TableConsistencyCheckReport.py
@@ -17,19 +17,21 @@ from email.mime.text import MIMEText
 now = datetime.now()
 today = now.strftime("%d-%m-%Y")
 
-# User Editable Area
+# Beginning of the User Editable Area
 hdbuserstore_key = "<KEY>"
-consistency_check_result_count = "SELECT COUNT(*) FROM M_CONSISTENCY_CHECK_HISTORY_ERRORS WHERE SEVERITY = 'HIGH';"
-consistency_check_error_count = "SELECT COUNT(*) FROM M_CONSISTENCY_CHECK_HISTORY_ERRORS WHERE SEVERITY = 'ERROR';"
-consistency_check_last_run = "SELECT TOP 1 * FROM (SELECT TO_VARCHAR(LAST_START_TIME, 'DD-MM-YYYY HH24:MI:SS') FROM M_CONSISTENCY_CHECK_HISTORY ORDER BY LAST_START_TIME DESC);"
 system_name = os.environ["SAPSYSTEMNAME"]
 smtp_server = "<SMTP HOST>"
 smtp_port = "<SMTP PORT>"
 email_from = "<FROM ADDRESS>"
 email_to = "<TO ADDRESS>"
 # email_cc = "<LIST OF ADDRESSES SEPERATED BY A COMMA>"
-subject = "Table consistency report for {ph_system_name} on {ph_today}".format(ph_system_name=system_name, ph_today=today)
 separator = "====================================================================="
+# End of the User Editable Area
+subject = "Table consistency report for {ph_system_name} on {ph_today}".format(ph_system_name=system_name, ph_today=today)
+consistency_check_result_count = "SELECT COUNT(*) FROM M_CONSISTENCY_CHECK_HISTORY_ERRORS WHERE SEVERITY = 'HIGH';"
+consistency_check_error_count = "SELECT COUNT(*) FROM M_CONSISTENCY_CHECK_HISTORY_ERRORS WHERE SEVERITY = 'ERROR';"
+consistency_check_last_run = "SELECT TOP 1 * FROM (SELECT TO_VARCHAR(LAST_START_TIME, 'DD-MM-YYYY HH24:MI:SS') FROM M_CONSISTENCY_CHECK_HISTORY ORDER BY LAST_START_TIME DESC);"
+
 
 # Create  a trace file and write any exceptions that have been caught to the trace file
 try:
